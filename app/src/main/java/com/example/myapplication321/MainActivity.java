670fddfa622fb162fcbe9b0fe25eb55fbf270aa8
Portuay <pas_ul15@hotmail.com>
2021-03-28 18:16:07 -0500

    cambios con nueva activity

--- app/src/main/java/com/example/myapplication321/MainActivity.java
+++ app/src/main/java/com/example/myapplication321/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.myapplication321;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -40,6 +41,13 @@ public class MainActivity extends AppCompatActivity {
                     finish();
                     startActivity(getIntent());
                 }
+            }
+        });
+        button_view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,ViewEmployeeActivity.class);
+                startActivity(intent);
             }
         });
 
---
+++ app/src/main/java/com/example/myapplication321/ViewEmployeeActivity.java
@@ -0,0 +1,14 @@
+package com.example.myapplication321;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ViewEmployeeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_employee);
+    }
+}
